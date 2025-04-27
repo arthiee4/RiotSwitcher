49f11eaf3cba69d19b864d41343ff48e30e6524f
arthiee4 <arthiee4@gmail.com>
2025-04-27 07:04:20 -0300

    some optimizations

--- src/systemtray.gd
+++ src/systemtray.gd
@@ -22,3 +22,10 @@ func _on_menu_id_selected(id: int) -> void:
 func _on_menu_id_pressed(button, _position):
 	if button == MOUSE_BUTTON_LEFT:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+## FPS limiter
+func _notification(what):
+	if what == NOTIFICATION_APPLICATION_FOCUS_OUT:
+		Engine.max_fps = 5  # Reduz para 10 FPS quando perde o foco
+	elif what == NOTIFICATION_APPLICATION_FOCUS_IN:
+		Engine.max_fps = 60  # Volta para 60 FPS quando volta o foco
